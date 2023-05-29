aa6b7c04970678437b032477554cda8150ff155f
左康生 <zhen0578@qq.com>
2023-05-29 14:51:36 +0800

    修改mj为art

--- request/index.ts
+++ request/index.ts
@@ -53,13 +53,13 @@ const streamFetch = async (
 };
 
 export const Imagine = (body: string, loading?: (uri: MJMessage) => void) => {
-  return streamFetch("/art/api/imagine", body, loading);
+  return streamFetch("api/imagine", body, loading);
 };
 
 export const Upscale = (body: string, loading?: (uri: MJMessage) => void) => {
-  return streamFetch("/art/api/upscale", body, loading);
+  return streamFetch("api/upscale", body, loading);
 };
 
 export const Variation = (body: string, loading?: (uri: MJMessage) => void) => {
-  return streamFetch("/art/api/variation", body, loading);
+  return streamFetch("api/variation", body, loading);
 };

62c65f8d96f8a528e020ca8ed15741bfd1d6d136
左康生 <zhen0578@qq.com>
2023-05-29 14:52:26 +0800

    修改mj为art

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const isDev = process.env.NODE_ENV === 'development';
 const nextConfig = {
-  basePath: '/uedmj',
+  basePath: '/uedart',
   reactStrictMode: true,
   transpilePackages: ['antd'],
-  assetPrefix: isDev ? '' : 'https://cdn.superx.chat/uedmj/',
+  assetPrefix: isDev ? '' : 'https://cdn.superx.chat/uedart/',
   trailingSlash: true,
   async rewrites() {
     return [

23d4e944b687b03176341b4c1e8cbcd206182ce5
左康生 <zhen0578@qq.com>
2023-07-06 07:31:10 +0800

    去掉百度统计

--- public/baidutongji.js
+++ public/baidutongji.js
@@ -1,10 +1,10 @@
-var _hmt = _hmt || [];
-(function () {
-  var hm = document.createElement('script');
-  hm.src = 'https://hm.baidu.com/hm.js?d938c62588f16d3f5ee8dcde67eef1a5';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(hm, s);
-})();
+// var _hmt = _hmt || [];
+// (function () {
+//   var hm = document.createElement('script');
+//   hm.src = 'https://hm.baidu.com/hm.js?d938c62588f16d3f5ee8dcde67eef1a5';
+//   var s = document.getElementsByTagName('script')[0];
+//   s.parentNode.insertBefore(hm, s);
+// })();
 //以诚智控
 // var _hmt = _hmt || [];
 // (function () {

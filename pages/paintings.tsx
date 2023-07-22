1ba26b913f1d081a5ad3918a73131074d5354e22
左康生 <zhen0578@qq.com>
2023-07-22 22:37:16 +0800

    在列表页添加bd_vid追踪

--- pages/paintings.tsx
+++ pages/paintings.tsx
@@ -1,7 +1,17 @@
 import ImgListPage from "../components/ImgListPage";
 import { ImgCardModel, ImgPageType } from '../scripts/types'
+import { getQueryString } from "../scripts/utils";
 
 const Paintings: React.FC = () => {
+    const setBDVid = (vid: string) => {
+        //从链接中取出bd_vid参数
+        // const url = new URL(window.location.href);
+        const bd_vid = getQueryString('bd_vid');
+        if (bd_vid) {
+            localStorage.setItem('bd_vid', bd_vid);
+        }
+    }
+
     return <ImgListPage type={ImgPageType.PUBLIC} />;
 }
 

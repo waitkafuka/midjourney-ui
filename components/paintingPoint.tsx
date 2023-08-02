083513d3588af54792c6a98fb9a21da5b6496c36
左康生 <zhen0578@qq.com>
2023-08-02 08:51:20 +0800

    修改价格改为58

--- components/paintingPoint.tsx
+++ components/paintingPoint.tsx
@@ -110,7 +110,7 @@ const PaingPoint = ({ }) => {
                     <div style={{ display: "flex", flexGrow: 1, lineHeight: 1.6, textAlign: "center", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
                         <div>1000 个点数 / {price} 元</div>
                         <div>可应用于 Stable Diffusion、Midjourney、DALLE、AI 艺术二维码，点数永久有效。</div>
-                        <div>每张图消耗 8 个点数，变体 4 个点数，高清放大 2 个点数。（SD 根据参数消耗不同点数，AI 艺术二维码 {qrcodeCost} 点数/每张）</div>
+                        <div>每张图消耗 8 个点数，变体 4 个点数，获取单张高清图 2 个点数。（SD 根据参数消耗不同点数，AI 艺术二维码 {qrcodeCost} 点数/每张）</div>
                         <div>（ midjourney四宫格算一张图）</div>
                     </div>
                 </div>
